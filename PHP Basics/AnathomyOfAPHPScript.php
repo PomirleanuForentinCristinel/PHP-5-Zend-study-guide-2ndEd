b0d360ed9b14feb5d6c7f722db2bb5c2f662772f
Pomirleanu Florentin-Cristinel <pomirleanu.florentin@aol.com>
2014-07-10 09:06:48 +0300

    PHP Basics-Have to add more

---
+++ PHP Basics/AnathomyOfAPHPScript.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+*	COMMENTS
+*/
+
+//	Single line comment
+
+#	Single line comment
+
+/*	Multi-line comments */
+
+/**
+ * API Documentation Example
+ * 
+ * @param string $bar
+ */
+	function foo($bar){
+
+	}
+
+/**
+ * NOTE
+ * 
+ * Both types of single line comments, //and #,can be ended using a new line (\r, \nor \r\n) or by ending the current PHP block using the PHP closing tag —?>. 
+ */
+
+/**
+ * WHITESPACE
+ */
+
+/**
+ * Can't have any whitespace between <? and php
+ * Can't breack apart keywords (ex.: whi le, fo r, and funct ion)
+?>
---
+++ PHP Basics/Syntax.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+*	Source Files and PHP Tags 
+*/
+
+/**
+*	Standard Tags <?php .. code ?>
+*/
+
+/**
+*	Short Tags <? .. code ?> <?= $variable ?>
+*/
+
+/**
+*	Script Tags <script language="php"> .. code </script>
+*/
+
+/**
+*	Asp Tags <% code .. %>
+*/
+
+/**	Note
+*	Short tags,script tags and ASP tags are all considered deprecated and their use is strongly discouraged
+*	An easy way to prevent spurious output from an include ﬁle is to omit the closing tag at the end,which the parser considers this perfectly legal.
+/*
+
+ ?>
